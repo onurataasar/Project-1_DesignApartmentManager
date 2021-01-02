5b27f1d4dbfc9314e33bd0daaa0899130a8d860c
Onur Ata Asar <onurataasar@gmail.com>
2021-01-02 21:15:28 +0300

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -4,28 +4,60 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <link rel="stylesheet" href="./css/style.css">
-
-
+  <link rel="stylesheet" href="./css/homepage.css">
   <title>Asar Apartments</title>
 </head>
-
-
-
 <body>
+  
   <header>
-    <div class="logo-container">
+  <div class="logo-container">
       <a href="homepage.php" style="text-decoration:none;color: white; padding:15px;   font: italic small-caps bold 12px/30px Georgia, serif;
  font-weight: 1000;margin:7% 7%; background-color: black;">
         <h3>Asar Residentials</h3>
       </a>
     </div>
-    <li>
-    <nav>
-      <ul class="nav-links" style="margin-left:-500px; padding:350px;">
+    
+   
+    <nav class="navbar sticky-top  navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Welcome</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="homepage.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="residents.html">Residents</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="https://www.google.com/maps/@36.6728673,30.5570388,16.24z"target="_blank">Navigation</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactus.html">Contact</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Login
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="login.php">User Login</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="adminlogin.php">Admin Login</a>
+        </div>
+      </li>
+    </ul>
+  
+  </div>
+</nav>
+
+  
+    <!-- <nav>
+      <ul class="nav-links" style="margin-right:200px; padding:0px;">
         <li><a class="nav-link" href="residents.html">Residents</a></li>
         <li><a class="nav-link" href="login.php">User Login</a></li>
         <li><a class="nav-link" href="adminlogin.php">Admin Login</a></li>
@@ -33,19 +65,18 @@
         <li><a class="nav-link" href="https://www.google.com/maps/@36.6728673,30.5570388,16.24z"target="_blank">Where Are We?</a></li>
       </ul>
     </nav>
+
+-->
   </header> <br>
+  
   <div class="row">
     <div class="col-sm-2">
     </div>
     <div class="col-sm-8">
       <br><br>
-
-
       <div>
         <h1 class="display-5">What are We Offering?<br><small></small></h1> <br>
-
         <hr class="featurette-divider">
-
         <div class="row featurette">
           <div class="col-md-7">
             <h2 class="featurette-heading">Our Pool</h2>
@@ -62,7 +93,6 @@
                   height: 350px;
                   width: 350px;
                 }
-
                 .canvas {
                   overflow: hidden;
                   background: none !important;
@@ -73,11 +103,7 @@
             </div>
           </div>
         </div>
-
-
-
         <hr class="featurette-divider">
-
         <div class="row featurette">
           <div class="col-md-7">
             <h2 class="featurette-heading">Garage</h2>
@@ -95,7 +121,6 @@
                   height: 450px;
                   width: 350px;
                 }
-
                 .canvas {
                   overflow: hidden;
                   background: none !important;
@@ -106,9 +131,7 @@
             </div>
           </div>
         </div>
-
         <hr class="featurette-divider">
-
         <div class="row featurette">
           <div class="col-md-7">
             <h2 class="featurette-heading">A Happy and Safe Life</h2>
@@ -126,7 +149,6 @@
                   height: 350px;
                   width: 350px;
                 }
-
                 .canvas {
                   overflow: hidden;
                   background: none !important;
@@ -140,7 +162,6 @@
         <p>
         </p>
       </div>
-
     </div>
   </div>
   <br> <br>
@@ -149,11 +170,9 @@
     <div class="footer-copyright text-center py-3">Asar Residents
     </div>
   </footer>
-
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-
   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -178,6 +197,4 @@
   </div>
   <br>
 </body>
-
-
 </html>
