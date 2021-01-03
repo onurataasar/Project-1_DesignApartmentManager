f1e26acff1f555f0cb2e01f581c6767c5596144a
Onur Ata Asar <onurataasar@gmail.com>
2021-01-04 00:16:22 +0300

    Add files via upload

---
+++ delete_user.php
@@ -0,0 +1,30 @@
+<?php
+/* 
+ DELETE.PHP
+ Deletes a specific entry from the 'players' table
+*/
+
+ // connect to the database
+ include "check.php";
+
+ // check if the 'id' variable is set in URL, and check that it is valid
+// Create connection
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$userID = $_GET['userID'];
+
+// sql to delete a record
+$sql = "DELETE FROM users WHERE userID='$userID'";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+  header("Location: admin.php");
+} else {
+  echo "Error deleting record: " . $conn->error;
+  header("Location: admin.php");
+}
+
+$conn->close();
+?>
---
+++ user.php
@@ -0,0 +1,124 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<html> <title> Welcome </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/user.css">
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+
+
+  <!--  <div class="logo-container">
+      <a href="homepage.php" style="text-decoration:none;color: white; padding:15px;   font: italic small-caps bold 12px/30px Georgia, serif;
+ font-weight: 1000;margin:7% 7%; background-color: black;">
+        <h3>Asar Residentials</h3>
+      </a>
+    </div>
+-->
+   
+    <nav class="navbar fixed-top  navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Welcome</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="homepage.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="residents.html">Residents</a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="https://www.google.com/maps/@36.6728673,30.5570388,16.24z"target="_blank">Navigation</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactus.html">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="homepage.php">Log Out</a>
+      </li>
+     
+    </ul>
+  
+  </div>
+</nav>
+</head>
+
+  <body>
+
+  <header>
+
+
+<div class="container">
+    <div class="row profile">
+		<div class="col-md-3">
+			<div class="profile-sidebar">
+
+				<!-- SIDEBAR USER TITLE -->
+				<div class="profile-usertitle">
+					<div class="profile-usertitle-name">
+
+					</div>
+					<div class="profile-usertitle-job">
+						Resident
+					</div>
+				</div>
+				<!-- END SIDEBAR USER TITLE -->
+				<!-- SIDEBAR BUTTONS -->
+				<div class="profile-userbuttons">
+					<button type="button" class="btn btn-success btn-sm">Follow</button>
+					<button type="button" class="btn btn-danger btn-sm">Message</button>
+				</div>
+				<!-- END SIDEBAR BUTTONS -->
+				<!-- SIDEBAR MENU -->
+				<div class="profile-usermenu">
+					<ul class="nav">
+						<li class="active">
+							<a href="#">
+							<i class="glyphicon glyphicon-home"></i>
+							Overview </a>
+						</li>
+						<li>
+							<a href="#">
+							<i class="glyphicon glyphicon-user"></i>
+							Account Settings </a>
+						</li>
+						<li>
+							<a href="#" target="_blank">
+							<i class="glyphicon glyphicon-ok"></i>
+							Tasks </a>
+						</li>
+						<li>
+							<a href="#">
+							<i class="glyphicon glyphicon-flag"></i>
+							Help </a>
+						</li>
+					</ul>
+				</div>
+				<!-- END MENU -->
+			</div>
+		</div>
+		<div class="col-md-9">
+            <div class="profile-content">
+			   Some user related content goes here...
+            </div>
+		</div>
+	</div>
+</div>
+<center>
+<strong>Asar Residentials</a></strong>
+</center>
+<br>
+<br>
+</header> <br>
+
