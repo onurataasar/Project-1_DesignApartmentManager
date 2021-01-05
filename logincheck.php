fc3deb6544fb905ace396ad3eff55887881d111f
Onur Ata Asar <onurataasar@gmail.com>
2021-01-05 21:03:34 +0300

    Update logincheck.php

--- logincheck.php
+++ logincheck.php
@@ -2,6 +2,7 @@
 <?php
 include "check.php";
 
+@session_start();
 
 if(isset($_POST['submit'])){
     if(empty($_POST['username']) || empty($_POST['psw'])) {
@@ -13,12 +14,19 @@ if(isset($_POST['submit'])){
         $pass = $_POST['psw'];
 
         $db=mysqli_select_db($conn,"users");
-        $query =mysqli_query($conn,"SELECT * FROM users WHERE psw='$pass' AND username='$uname'");
+        $query =mysqli_query($conn,"SELECT * FROM users WHERE psw= MD5('$pass') AND username='$uname'");
 
         $rows = mysqli_num_rows($query);
         if($rows == 1){
-            header("Location: homepage.php");
-            exit();
+            // Storing username in session variable 
+            $_SESSION['username'] = $uname; 
+              
+            // Welcome message 
+            $_SESSION['success'] = "You have logged in!"; 
+              
+            // Page on which the user is sent 
+            // to after logging in 
+            header('location: user.php'); 
         }
         
         else{
