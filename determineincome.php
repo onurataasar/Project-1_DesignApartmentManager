344fecf8729aa203f0bf3f656fc5844f478618a5
Onur Ata Asar <onurataasar@gmail.com>
2021-01-29 22:58:58 +0300

    Add files via upload

---
+++ determineincome.php
@@ -0,0 +1,179 @@
+<!-- Logout button -->
+<?php
+if (isset($_GET['logout'])) {
+	session_destroy();
+	unset($_SESSION['adminUsername']);
+	header("location: adminlogin.php");
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<html>
+<title> Welcome </title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="userpage.css">
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+
+
+
+<nav class="navbar fixed-top  navbar-expand-lg navbar-dark bg-dark">
+	<a class="navbar-brand" href="#">Welcome to Admin Page</a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="homepage.php">Home <span class="sr-only">(current)</span></a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="https://www.google.com/maps/@36.6728673,30.5570388,16.24z" target="_blank">Navigation</a>
+			</li>
+
+		</ul>
+
+	</div>
+</nav>
+</head>
+
+<body>
+
+	<header>
+
+
+		<div class="container">
+			<div class="row profile">
+				<div class="col-md-3">
+					<div class="profile-sidebar">
+
+						<!-- SIDEBAR USER TITLE -->
+						<div class="profile-usertitle">
+							<div class="profile-usertitle-name">
+								<?php
+								echo "Welcome onurataasar";
+								?>
+							</div>
+							<div class="profile-usertitle-job">
+								Admin
+							</div>
+						</div>
+						<!-- END SIDEBAR USER TITLE -->
+						<!-- SIDEBAR BUTTONS -->
+						<div class="profile-userbuttons">
+							<button type="button" class="btn btn-success btn-sm"><a href="add_user.php" style="color:white;">ADD RESIDENT</button>
+							<button type="button" style="color:white;" class="btn btn-danger btn-sm"> <a href="admin.php?logout='1'" style="color:white;">Logout</button>
+						</div>
+						<!-- END SIDEBAR BUTTONS -->
+						<!-- SIDEBAR MENU -->
+						<div class="profile-usermenu">
+							<ul class="nav">
+								<li class="active">
+									<a href="paymentlist.php">
+										<i class="glyphicon glyphicon-home"></i>
+										Payment List </a>
+								</li>
+								<li>
+									<a href="determineincome.php">
+										<i class="glyphicon glyphicon-user"></i>
+										Determine Income</a>
+								</li>
+								<li>
+									<a href="determineexpense.php">
+										<i class="glyphicon glyphicon-user"></i>
+										Determine Rate/Expense </a>
+								</li>
+
+								<li>
+									<a href="userlist.php">
+										<i class="glyphicon glyphicon-flag"></i>
+										Residents </a>
+								</li>
+							</ul>
+						</div>
+						<!-- END MENU -->
+					</div>
+				</div>
+				<div class="col-md-9">
+					<div class="profile-content">
+						<h2>Determine Monthly Incomes</h2>
+<br>
+<?php
+    include "check.php";
+
+    if (isset($_POST['submit']) && !empty($_POST['income']) && !empty($_POST['incomename'])) {
+      $income = $_POST['income'];
+      $incomename = $_POST['incomename'];
+
+      $sql = "INSERT INTO incomes (income, incomename) VALUES ('$income', '$incomename')";
+
+
+
+      if ($conn->query($sql) === TRUE) {
+        echo "";
+      } else {
+        echo "";
+      }
+      header('location:admin.php');
+    }
+
+    ?>
+    <form class="form-signin" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
+                                                  ?>" method="post">
+
+      <table>
+
+       
+        <div class="textbox">
+          <tr>
+            <td>Income Name:(*):</td>
+            <td><input type="text" name="incomename" value="" placeholder="Name of your income" size="50" required>
+              <p style="color:red;display:inline;"></p>
+            </td>
+          </tr>
+        </div> 
+        <div class="textbox">
+          <tr>
+            <td>Income </td>
+            <td><input type="int" name="income" value="" placeholder="Amount" size="50" required>
+              <p style="color:red;display:inline;"></p>
+            </td>
+          </tr>
+        </div>
+
+      </table> <br>
+      <?php if (isset($_GET['error'])) { ?>
+        <p class="error"><?php echo $_GET['error']; ?></p>
+      <?php } ?>
+
+      <input class="btn" type="submit" class="btn btn-primary" name="submit" value="Send"> <br> <br>
+
+
+    </form>
+	<br></br>
+
+  </div>
+
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<center>
+
+			<strong>Asar Residentials</a></strong>
+		</center>
+		<br>
+		<br>
+	</header> <br>
